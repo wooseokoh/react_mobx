ce4090a39378c9703efcef9fa47aacd58b3426ee
wooseokoh <allsafe1206@gmail.com>
2022-09-23 17:25:43 +0900

    4.counter-mobx complete

---
+++ 4.counter-mobx/src/component/CounterComponent.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import { Button, Box } from "@material-ui/core";
+import { inject, observer } from "mobx-react";
+
+@inject("counterStore")
+@observer
+class CounterComponent extends Component {
+  render() {
+    const { counterStore } = this.props;
+    return (
+      <div>
+        <Button
+          onClick={() => counterStore.decrement()}
+          variant='contained'
+          color='primary'
+          size='large'
+        >
+          -
+        </Button>
+
+        <Box component='span' m={5}>
+          {counterStore._count}
+        </Box>
+
+        <Button
+          onClick={() => counterStore.increment()}
+          variant='contained'
+          color='primary'
+          size='large'
+        >
+          +
+        </Button>
+      </div>
+    );
+  }
+}
+
+export default CounterComponent;
